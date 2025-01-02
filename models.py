e1301cea54845625914bb67e4992d357f80ccbf0
Roberto Fierimonte <roberto.fierimonte@gmail.com>
2025-01-02 16:18:24 +0200

    Trained reasonable model
    Signed-off-by: Roberto Fierimonte <roberto.fierimonte@gmail.com>

--- models.py
+++ models.py
@@ -3,11 +3,13 @@ import torch.nn as nn
 import torch.nn.init as I
 
 
-class Net(nn.Module):
+class VGGNet(nn.Module):
+    """Deep neural network modelled after the VGG-16 architecture."""
 
     def __init__(self):
-        super(Net, self).__init__()
+        super(VGGNet, self).__init__()
         
+        # Feature extraction block
         self.features = nn.Sequential(
             # Block 1
             nn.Conv2d(1, 64, 3, padding=1),
@@ -68,7 +70,9 @@ class Net(nn.Module):
             nn.MaxPool2d(2, 2),
             # Output size: N x 7 x 7 x 512
         )
-        self.classifier = nn.Sequential(
+
+        # MLP block for regression output
+        self.mlp = nn.Sequential(
             nn.Linear(7 * 7 * 512, 4096, bias=True),
             nn.BatchNorm1d(4096),
             nn.ReLU(inplace=True),
@@ -77,10 +81,10 @@ class Net(nn.Module):
             nn.BatchNorm1d(4096),
             nn.ReLU(inplace=True),
             nn.Dropout1d(0.5, inplace=False),
-            nn.Linear(4096, 136, bias=True)
+            nn.Linear(4096, 136, bias=True),
         )  # Output size: N x 136
 
-        # Add weight initialisation
+        # Add weights initialisation
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 I.xavier_normal_(m.weight)
@@ -97,5 +101,56 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.features(x)
         x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        x = self.mlp(x)
+        return x
+
+
+class Net(nn.Module):
+    """Single deep neural network for keypoint recognition."""
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # Feature extraction block
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, 5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, 5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, 5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, 5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+        )  # Output size: N x 3 x 3 x 256
+
+        # MLP block for regression output
+        self.mlp = nn.Sequential(
+            nn.Linear(2304, 1024, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2, inplace=False),
+            nn.Linear(1024, 136, bias=True),
+        )  # Output size: N x 136
+
+        # Add weights initialisation
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                I.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    I.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                I.xavier_normal_(m.weight)
+                I.constant_(m.bias, 0)
+            
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.mlp(x)
         return x
